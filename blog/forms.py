442ea0fba36f98b6370a6bef55bf0fe1b6d74269
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-23 23:11:08 +0300

    Added new functional for create post via web interface

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Tag
+from .models import Tag, Post
 
 
 class TagForm(forms.ModelForm):
@@ -22,4 +22,24 @@ class TagForm(forms.ModelForm):
         if Tag.objects.filter(slug__iexact=new_slug):
             raise ValidationError(f'We already have tag {new_slug}')
 
+        return new_slug
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
         return new_slug
