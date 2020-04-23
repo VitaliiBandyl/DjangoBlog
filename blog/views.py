e6354cbfe57190bb30b2316346977b6e29a770ae
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-23 19:17:16 +0300

    Added new functional for create tag from web interface

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views.generic import View
-from django.shortcuts import get_object_or_404
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 # Create your views here.
 def posts_list(request):
@@ -14,9 +14,6 @@ def posts_list(request):
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
-    # def get(self, request, slug):
-    #     post = get_object_or_404(Post, slug__iexact=slug)
-    #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 def tags_list(request):
@@ -27,6 +24,17 @@ def tags_list(request):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
-    # def get(self, request, slug):
-    #     tag = get_object_or_404(Tag, slug__iexact=slug)
-    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
